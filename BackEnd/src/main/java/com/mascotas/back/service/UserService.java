224db548d8865eb18be659bbc0d621051cb284db
ARGEEK1 <antonio198889@gmail.com>
2023-11-30 18:04:02 +0100

    update

--- BackEnd/src/main/java/com/mascotas/back/service/UserService.java
+++ BackEnd/src/main/java/com/mascotas/back/service/UserService.java
@@ -13,5 +13,5 @@ public interface UserService {
     public List<UserResponseDto> getUsers();
     public User createUser (User user);
     public void deleteUser (Long id);
-    public User findUserById (Long id);
+    public UserResponseDto findUserById (Long id);
 }
