beb2b120aeea39d70aa3e4a13b25babc95dfe870
jrlopes <contacto.juniorlope@gmail.com>
2023-12-01 12:02:01 -0500

    login y navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif']},
+    colors:{
+      "red-primary": '#BB2649',
+      "beige-secondary": '#FFD6A5'
+    },
     extend: {},
   },
   plugins: [],
