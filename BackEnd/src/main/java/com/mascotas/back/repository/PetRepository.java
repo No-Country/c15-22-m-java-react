d984769fa1ae527f837b1b91e2eea5265fcdd386
Mathias Reid <mathyoyo@hotmail.es>
2023-12-04 11:06:01 -0300

    subo repos

--- BackEnd/src/main/java/com/mascotas/back/repository/PetRepository.java
+++ BackEnd/src/main/java/com/mascotas/back/repository/PetRepository.java
@@ -1,12 +1,71 @@
 package com.mascotas.back.repository;
 
 import com.mascotas.back.model.Pet;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PetRepository extends  JpaRepository <Pet, Long>{
     Page<Pet> findAll(Pageable paginacion);
+    
+    @Query("SELECT p FROM Pet")
+    List<Pet> listaPets();
+    
+    @Query("SELECT p FROM Pet WHERE p.state = ADOPTED OR p.state = FOUND")
+    Optional<Pet> buscarMascotasConFinalFeliz();
+    
+    @Query("SELECT p FROM Pet WHERE p.state = LOST")
+    Optional<Pet> buscarMascotasPerdidas();
+    
+    @Query("SELECT p FROM Pet WHERE p.state = ADOPTION")
+    Optional<Pet> buscarMascotasEnAdopcion();
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.name = :name WHERE p.id = :id")
+    void modificarMascotaNombre(@Param("name")String name, @Param("id")Long id);
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.description = :description WHERE p.id = :id")
+    void modificarMascotaDescription(@Param("description")String description, @Param("id")Long id);
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.type = :type WHERE p.id = :id")
+    void modificarMascotaType(@Param("type")String type, @Param("id")Long id);
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.race = :race WHERE p.id = :id")
+    void modificarMascotaRace(@Param("race")String race, @Param("id")Long id);
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.age = :age WHERE p.id = :id")
+    void modificarMascotaAge(@Param("age")String age, @Param("id")Long id);
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.state = :state WHERE p.id = :id")
+    void modificarMascotaState(@Param("state") String state, @Param("id")Long id);
+    
+    @Modifying
+    @Query("UPDATE Pet p SET p.name = :name , p.description = :description"
+            + ", p.type = :type , p.race = :race "
+            + ", p.age = :age , p.state = :state "
+            + "p.state = :state WHERE p.id = :id"
+    )
+    void modificarPet(@Param("name") String name,
+            @Param("description") String description,
+            @Param("type") String type,
+            @Param("race") String race,
+            @Param("age") String age,
+            @Param("state") String state,
+            @Param("id")Long id);
+    
+         void deleteById(Long id);
+
+    
 }
--- BackEnd/src/main/java/com/mascotas/back/repository/UserRepository.java
+++ BackEnd/src/main/java/com/mascotas/back/repository/UserRepository.java
@@ -1,11 +1,65 @@
 package com.mascotas.back.repository;
 
 import com.mascotas.back.model.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
+    
+    
+    @Query("SELECT u FROM User")
+    List<User> buscarUser();
+    
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> BuscarUserPorId(@Param("id")Long id);
+    
+     @Query("SELECT u FROM User u WHERE u.name = :name")
+    List<User> buscarUserPorNombre(@Param("name") String nombre);
+    
+     @Modifying
+    @Query("UPDATE User u SET u.name = :name WHERE u.id = :id")
+    void modificarNombre(@Param("name") String name, @Param("id") Long id);
+    
+    
+    @Modifying
+    @Query("UPDATE Uset u SET u.email = :email WHERE u.id = :id")
+    void modificarEmail(@Param("email") String email, @Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.password = :password WHERE u.id = :id")
+    void modificarPassword(@Param("password") String password, @Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.phone = :phone WHERE u.id = :id")
+    void modificarPhone(@Param("phone") String phone, @Param("id") Long id);
+    
+     @Modifying
+    @Query("UPDATE User u SET u.role = :role WHERE u.id = :id")
+    void modificarRole(@Param("role") String role, @Param("id") Long id);
+    
+    @Modifying
+    @Query("UPDATE User u SET u.pets = :pets WHERE u.id = :id")
+    void modificarPets(@Param("pets") String pets, @Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.name = :name , u.email = :email,"
+            + " u.password = :password, u.phone = :phone,"
+            + " u.role = :role , u.pets = :pets"
+            + " WHERE u.id = :id")
+    void modificarUser(@Param("nombre") String nombre,
+                          @Param("email") String email,
+                          @Param("password") String password,
+                          @Param("phone") String phone,
+                          @Param("role") String role,
+                          @Param("pets") String pets,
+                          @Param("id") Long id);
+    
+    void deleteById(Long id);
 }
