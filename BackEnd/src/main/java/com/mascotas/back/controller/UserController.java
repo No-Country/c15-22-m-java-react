cd9de9515e6877fd7581554e1a136ee7268abe17
ARGEEK1 <antonio198889@gmail.com>
2023-12-11 16:01:25 +0100

    add controller user profile

--- BackEnd/src/main/java/com/mascotas/back/controller/UserController.java
+++ BackEnd/src/main/java/com/mascotas/back/controller/UserController.java
@@ -3,6 +3,7 @@ package com.mascotas.back.controller;
 
 import com.mascotas.back.dto.UserResponseDto;
 import com.mascotas.back.model.User;
+import com.mascotas.back.security.jwt.JwtService;
 import com.mascotas.back.service.UserService;
 
 import java.util.List;
@@ -11,15 +12,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpHeaders;
 
 @RestController
 @RequestMapping("/user")
@@ -29,12 +25,35 @@ public class UserController {
     @Autowired
     private UserService userServ;
 
+    @Autowired
+    private JwtService jwtService;
+
     @GetMapping
     @ResponseBody
     public List<UserResponseDto> getUsers() {
         return userServ.getUsers();
     }
 
+    @GetMapping("/profile")
+    public ResponseEntity<?> userProfile(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+        String userName = null;
+        if (StringUtils.hasText(token) && token.startsWith("Bearer ")) {
+            userName = jwtService.getUsernameFromToken(
+                    token.substring(7)
+            );
+        }
+
+        Optional<User> user = userServ.findUserByEmail(userName);
+
+        if (user.isPresent()) {
+            UserResponseDto userProfile = new UserResponseDto(user.get());
+            return ResponseEntity.ok(userProfile);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("Usuario no encontrado");
+        }
+    }
 
 
     @PostMapping
@@ -57,14 +76,13 @@ public class UserController {
             return ResponseEntity.ok(user);
         }
         return ResponseEntity
-                    .status(HttpStatus.NOT_FOUND)
-                    .body("Usuario no encontrado para el ID: " + id);
+                .status(HttpStatus.NOT_FOUND)
+                .body("Usuario no encontrado para el ID: " + id);
     }
 
     @GetMapping("/email/{email}")
     public ResponseEntity<Optional<User>> getUserByEmail(@PathVariable String email) {
         return ResponseEntity.ok(userServ.findUserByEmail(email));
     }
-
 
 }
