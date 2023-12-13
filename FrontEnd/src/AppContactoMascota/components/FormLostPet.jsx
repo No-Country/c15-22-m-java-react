4ce3be4c869ebcc8a96ba1f71eb2091c7aeab4e5
goraydev <jofre1236@gmail.com>
2023-12-12 19:53:41 -0500

    Actualizando formulario de mascotas

--- FrontEnd/src/AppContactoMascota/components/FormLostPet.jsx
+++ FrontEnd/src/AppContactoMascota/components/FormLostPet.jsx
@@ -168,7 +168,7 @@ export const FormLostPet = () => {
         type="submit"
         className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
       >
-        Publicar
+        Reportar
       </button>
     </form>
   );
