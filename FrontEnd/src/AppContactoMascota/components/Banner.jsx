10174103b7b93c01d71141204724be4fc4a969db
goraydev <jofre1236@gmail.com>
2023-12-14 10:28:56 -0500

    Mejorando el banner

--- FrontEnd/src/AppContactoMascota/components/Banner.jsx
+++ FrontEnd/src/AppContactoMascota/components/Banner.jsx
@@ -2,26 +2,18 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="bg-red-primary  pt-10 grid gap-10 justify-center grid-cols-[repeat(auto-fit,_260px)] max-w-[1240px] mx-auto items-center pb-7">
+    <section className="bg-red-primary">
+      <h1 className="text-5xl font-bold text-center text-white pt-6">
+        Últimas mascotas reportadas
+      </h1>
+      <div className="grid gap-10 justify-center grid-cols-[repeat(auto-fit,_260px)] max-w-[1240px] mx-auto items-center py-16">
+        {/* ----> Acá va renderizar las ultimas mascotas reportadas <------  */}
 
-       {/* ----> Acá va renderizar las ultimas mascotas reportadas <------  */}
-
-      <img
-        className="max-h-[500px]"
-        src="/images/nua.png"
-        alt=""
-      />
-      <img
-        className=" max-h-[500px]"
-        src="/images/nua.png"
-        alt=""
-      />
-      <img
-        className=" max-h-[500px]"
-        src="/images/nua.png"
-        alt=""
-      />
-    </div>
+        <img className="max-h-[800px]" src="/images/nua.png" alt="" />
+        <img className=" max-h-[800px]" src="/images/nua.png" alt="" />
+        <img className=" max-h-[800px]" src="/images/nua.png" alt="" />
+      </div>
+    </section>
   );
 };
 
--- FrontEnd/src/AppContactoMascota/components/ListPets.jsx
+++ FrontEnd/src/AppContactoMascota/components/ListPets.jsx
@@ -5,7 +5,7 @@ const ListPets = () => {
   const { pets } = useContactoMascota();
 
   return (
-    <main className="bg-orange-100 py-6">
+    <main className="bg-orange-100 py-16">
       <h1 className="text-5xl font-bold text-center mb-5">Mascotas</h1>
       <section className="grid gap-6 justify-between m-0 grid-cols-[repeat(auto-fit,_260px)] max-w-[1200px] mx-auto bg-orange">
         {pets.map((pet) => (
