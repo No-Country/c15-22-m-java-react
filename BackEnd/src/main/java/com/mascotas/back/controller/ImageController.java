b6c6e9f9ca63eae2ea54a53a98fd4082dbf644af
Ramon <ramon.cruz187@gmail.com>
2023-12-19 22:11:21 -0300

    CrossOrigin imageController

--- BackEnd/src/main/java/com/mascotas/back/controller/ImageController.java
+++ BackEnd/src/main/java/com/mascotas/back/controller/ImageController.java
@@ -29,7 +29,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "localhost:5173")
+@CrossOrigin (origins="http://localhost:5173")
 public class ImageController {
 
     private final ImageService imageService;
