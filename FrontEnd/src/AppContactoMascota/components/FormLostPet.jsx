4ddfa89d9db07e063e10b3802656470fc4e486dc
goraydev <jofre1236@gmail.com>
2023-12-21 18:34:33 -0500

    camnbios

--- FrontEnd/src/AppContactoMascota/components/FormLostPet.jsx
+++ FrontEnd/src/AppContactoMascota/components/FormLostPet.jsx
@@ -3,6 +3,7 @@ import useForm from "../hooks/useForm";
 import { toBase64 } from "../helpers/toBase64";
 import { useContactoMascota } from "../../hooks/useContactoMascota";
 import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const FormLostPet = () => {
   const { reportPet, user } = useContactoMascota();
@@ -23,9 +24,16 @@ export const FormLostPet = () => {
     user_id: "",
   });
 
+  const navigate = useNavigate("");
+
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
     setFileInputValue(file);
+  };
+
+  const onClose = (t) => {
+    toast.dismiss(t.id);
+    navigate("/auth/dashboard");
   };
 
   const handleSubmit = (e) => {
@@ -45,13 +53,38 @@ export const FormLostPet = () => {
       reportPet(formState);
     });
 
-    toast.success("Mascota reportada", { duration: 4000 });
+    toast.custom((t) => (
+      <div
+        className={`${
+          t.visible ? "animate-enter" : "animate-leave"
+        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      >
+        <div className="flex-1 w-0 p-4">
+          <div className="flex items-start">
+            <div className="ml-3 flex-1">
+              <p className="text-sm font-medium text-gray-900">
+                Mascota reportada
+              </p>
+              <p className="mt-1 text-sm text-gray-500">Gracias por reportar</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex border-l border-gray-200">
+          <button
+            onClick={() => onClose(t)}
+            className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    ));
     onResetForm();
   };
 
   return (
     <form className="max-w-lg mx-auto" onSubmit={handleSubmit}>
-      <Toaster position="top-right" reverseOrder={false} />
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="mb-5">
         <label
           htmlFor="email"
@@ -109,7 +142,6 @@ export const FormLostPet = () => {
           <option value="Gato">Gato</option>
           <option value="Otro">Otro</option>
         </select>
-
       </div>
       <div className="mb-5">
         <label
