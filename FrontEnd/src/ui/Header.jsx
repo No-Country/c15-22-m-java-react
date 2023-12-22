027cedada484700bc848911160e0f3f30c40ba84
goraydev <jofre1236@gmail.com>
2023-12-21 19:46:13 -0500

    Arreglando el header

--- FrontEnd/src/ui/Header.jsx
+++ FrontEnd/src/ui/Header.jsx
@@ -82,10 +82,10 @@ const Header = () => {
         </div>
 
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className="items-center justify-between w-full md:flex md:w-auto md:order-1"
           id="navbar-cta"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-500 md:dark:bg-gray-900 dark:border-gray-500">
+          <ul className="flex flex-col gap-4 md:gap-0 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-500 md:dark:bg-gray-900 dark:border-gray-500">
             <li>
               <Link
                 to={"/"}
